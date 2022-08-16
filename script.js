7012fe20edbc67e372ad3386fa3489373b52f7b0
rolzar <otakurolando@gmail.com>
2022-08-16 01:44:20 -0700

    Change display according to numbers pressed

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container');
+const display = document.querySelector('.display')
 
 function add(a,b){
     return a + b
@@ -20,8 +21,21 @@ function thrCanOnlyBeTwo(){
 
 }
 
+
 function compute(e){
-    console.log(e);
+    const choose = e.target.classList.value
+    if(!choose.includes('btn')) return;
+    console.log(choose);
+
+    if(choose.includes('num')){
+        if(display.innerText == '0'){
+            display.innerText = e.target.innerText
+        } else{
+            display.innerText += e.target.innerText
+        }
+        
+    }
+
 
 }
 
