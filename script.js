4234661641e4f86c578e8a8bfcb8c5bc438e734d
rolzar <otakurolando@gmail.com>
2022-08-15 22:12:58 -0700

    Add initial calculator design

---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('.container');
+
+function add(a,b){
+    return a + b
+}
+
+function subtract(a,b){
+    return a - b
+}
+
+function multiply(a,b){
+    return a * b
+}
+
+function divide(a,b){
+    return a / b
+}
+
+function thrCanOnlyBeTwo(){
+
+}
+
+function compute(e){
+    console.log(e);
+
+}
+
+window.addEventListener('click',compute)
