793c526b1802b86171da01c34fc165e76e494d32
rolzar <otakurolando@gmail.com>
2022-08-16 22:12:05 -0700

    Store two separate numbers and clear function

--- script.js
+++ script.js
@@ -1,41 +1,72 @@
 const container = document.querySelector('.container');
 const display = document.querySelector('.display')
+let thrCanOnlyBeTwo = {}
+let isMaster = false;
 
-function add(a,b){
-    return a + b
+function add(){
+    return thrCanOnlyBeTwo.master + thrCanOnlyBeTwo.apprentice
 }
 
-function subtract(a,b){
-    return a - b
+function subtract(){
+    return thrCanOnlyBeTwo.master - thrCanOnlyBeTwo.apprentice
 }
 
-function multiply(a,b){
-    return a * b
+function multiply(){
+    return thrCanOnlyBeTwo.master * thrCanOnlyBeTwo.apprentice
 }
 
-function divide(a,b){
-    return a / b
+function divide(){
+    return thrCanOnlyBeTwo.master / thrCanOnlyBeTwo.apprentice
 }
 
-function thrCanOnlyBeTwo(){
+
+function changeDisplay(event){
+    if(display.innerText == '0' || isMaster){
+        display.innerText = event
+        isMaster = false;
+    } else{
+        display.innerText += event
+    }
 
 }
 
+function storeMaster(symbol){
+    thrCanOnlyBeTwo.math = symbol;
+    if('master' in thrCanOnlyBeTwo){
+        //isMaster = true; 
+    } else {
+        thrCanOnlyBeTwo.master = +display.innerText;
+        isMaster = true; 
+    }
+
+}
+
+function alter(event){
+    if(event == 'clear'){
+        display.innerText = 0;
+        thrCanOnlyBeTwo = {};
+    }
+}
+
+function equate(){
+    thrCanOnlyBeTwo.apprentice = +display.innerText;
+    console.log(thrCanOnlyBeTwo);
+
+}
 
 function compute(e){
     const choose = e.target.classList.value
     if(!choose.includes('btn')) return;
-    console.log(choose);
 
     if(choose.includes('num')){
-        if(display.innerText == '0'){
-            display.innerText = e.target.innerText
-        } else{
-            display.innerText += e.target.innerText
-        }
-        
+        changeDisplay(e.target.innerText)
+    } else if(choose.includes('math')){
+        storeMaster(e.target.id)
+    } else if(choose.includes('alter')){
+        alter(e.target.id)
+    } else if(choose.includes('equate')){
+        equate()
     }
-
 
 }
 
